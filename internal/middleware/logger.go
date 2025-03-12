3b76d8ca1ae2d7c8cb1ba3e1ad72a8683f82d147
Jason Horvath <jason@greaterdevelopment.com>
2025-03-11 23:39:37 -0400

    add log output to test middleware order

--- internal/middleware/logger.go
+++ internal/middleware/logger.go
@@ -9,6 +9,7 @@ import (
 func Logger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Add logging logic here
+		log.Println("LOGGER")
 		start := time.Now()
 		next.ServeHTTP(w, r)
 		log.Println(r.Method, r.URL.Path, time.Since(start))
