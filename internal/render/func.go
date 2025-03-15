0dc822d4603baf17f84efbad4f48bc79b245ef65
Jason Horvath <jason@greaterdevelopment.com>
2025-03-15 01:32:45 -0400

    rename funcs

--- internal/render/func.go
+++ internal/render/func.go
@@ -8,9 +8,9 @@ import (
 
 func TemplateFuncs() template.FuncMap {
 	return template.FuncMap{
-		"fn_assets_head": 	 AssetsHead,
-		"fn_is_development": func() bool { return config.App.IsDevelopment() },
-		"fn_is_production":	 func() bool { return config.App.IsProduction() },
-		"fn_url":			 func(path string) string { return config.App.FullURL(path) },
+		"assetsHead": 	 AssetsHead,
+		"isDevelopment": func() bool { return config.App.IsDevelopment() },
+		"isProduction":	 func() bool { return config.App.IsProduction() },
+		"url":		 	 func() string { return config.App.URL },
 	}
 }
