e6b2c315fc4224876c16bbb0d1c93e085fbe72d0
Jason Horvath <jason@greaterdevelopment.com>
2025-03-18 01:19:27 -0400

    improved view rendering

--- internal/render/view.go
+++ internal/render/view.go
@@ -1,209 +1,125 @@
 package render
 
 import (
+	"bytes"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+
+	"gohst/internal/utils"
 )
 
-type ViewProps struct {
-    ViewsDir      	string
-    ComponentsDir   string
-	LayoutsDir      string
-	ContentDir		string
-	TemplatesDir	string
+type ViewData struct {
+    Props    interface{}
+    Content  template.HTML
 }
 
 type View struct {
-	componentsDir 	string
-	layoutsDir		string
-	Template 		*template.Template
-	templatesDir 	string
-	viewsDir		string
-	contentDir		string
+	Template 	*template.Template
+	Layout  	string
+	Dirs 		ViewDirs
 }
 
-func NewView(props ViewProps) *View {
+type ViewDirs struct {
+	Layouts 	string
+	Templates 	string
+	Views 		string
+	Partials 	string
+	Components 	string
+}
 
-	if props.ViewsDir == "" {
-		props.ViewsDir = "views"
+func NewView() *View {
+	templateFuncs := TemplateFuncs()
+	view := &View{
+		Template: template.New("").Funcs(templateFuncs),
+		Layout: "layout/default",
+		Dirs: ViewDirs{
+			Layouts: "layouts",
+			Templates: "templates",
+			Views: "views",
+			Partials: "partials",
+			Components: "components",
+		},
 	}
 
-	if props.ComponentsDir == "" {
-		props.ComponentsDir = "components"
-	}
+	view.Init()
 
-	if props.LayoutsDir == "" {
-		props.LayoutsDir = "layouts"
-	}
-
-	if props.TemplatesDir == "" {
-		props.TemplatesDir = "templates"
-	}
-
-	return &View{
-		componentsDir: 	props.ComponentsDir,
-		layoutsDir: 	props.LayoutsDir,
-		Template:		template.New(""),
-		templatesDir: 	props.TemplatesDir,
-		viewsDir: 		props.ViewsDir,
-		contentDir: 	props.ContentDir,
-	}
+	return view
 }
 
 func (v *View) Init() {
-	v.LoadAllTemplates()
-	v.LoadLayouts()
-	v.LoadContentTemplates()
-	log.Println("VIEW TEMPLATES:" , v.Template.DefinedTemplates())
+	v.loadAll()
 }
 
-func (v *View) LoadComponentsDir() {
-	v.LoadTemplateDir(v.GetComponentsPath())
+func (v *View) LoadTemplates() {
+	// Go’s ParseGlob does not support brace expansion.
+	// Load HTML and tmpl files separately if needed.
+	_, err := v.Template.ParseGlob("templates/**/*.html")
+	if err != nil {
+		log.Fatalf("Error parsing HTML templates: %v", err)
+	}
+	_, err = v.Template.ParseGlob("templates/**/*.tmpl")
+	if err != nil {
+		log.Fatalf("Error parsing Tmpl templates: %v", err)
+	}
 }
 
-func (v *View) LoadLayoutsDir() {
-	v.LoadTemplateDir(v.GetLayoutsPath())
-}
-
-func (v *View) LoadContentTemplates() {
-	v.LoadTemplateDir(v.GetContentPath())
-}
-
-func (v * View) LoadAllTemplates() {
-    var allFiles []string
-
-	err := filepath.WalkDir(v.GetTemplatesPath(), func(path string, d os.DirEntry, err error) error {
-		log.Println("Walking through", path)
+func (v *View) loadAll() {
+	dirPath := v.Dirs.Templates
+	err := filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
-		if !d.IsDir() && filepath.Ext(path) == ".html" {
-			allFiles = append(allFiles, path)
+		if !info.IsDir() {
+			content, err := os.ReadFile(path)
+			if err != nil {
+				return err
+			}
+
+			name, err := filepath.Rel(dirPath, path)
+			if err != nil {
+				log.Fatal("No rel base path found.", err)
+			}
+
+			// Standardize path and remove extension
+			name = filepath.ToSlash(name)
+			name = name[:len(name)-len(filepath.Ext(name))]
+
+			// Parse into base template
+			_, err = v.Template.New(name).Parse(string(content))
+			if err != nil {
+				return err
+			}
 		}
 		return nil
 	})
-	log.Println("All files:", allFiles)
+
 	if err != nil {
-		log.Fatalf("Error walking through templates directory: %v", err)
+		log.Fatalf("Error loading tempaltes: %v", err)
 	}
 
-	templates, err = v.Template.ParseFiles(allFiles...)
+	log.Println("Loaded Dir:", dirPath, v.Template.DefinedTemplates())
+}
+
+func (v *View) Render(r http.ResponseWriter, viewName string, data ...interface{}) error {
+	var viewContent bytes.Buffer
+	useData := utils.StructEmpty(data)
+	log.Printf("useData type: %T", useData)
+	useViewName := v.Dirs.Views + "/" + viewName
+	err := v.Template.ExecuteTemplate(&viewContent, useViewName, useData)
 	if err != nil {
-		log.Fatalf("Error parsing templates: %v", err)
-	}
-}
-
-func (v * View) LoadTemplateDir(dir string, ext ...string) *template.Template{
-    templateExt := "*.html"
-
-    if len(ext) > 0 && ext[0] != "" {
-        templateExt = ext[0]
-    }
-
-	pattern := filepath.Join(dir, templateExt)
-	log.Println("Loading templates from directory", pattern)
-    templates = template.Must(template.ParseGlob(pattern))
-
-	var err error
-    templates, err = template.ParseGlob(pattern)
-    if err != nil {
-        log.Fatalf("Error loading templates from directory %s: %v", dir, err)
-    }
-
-	return templates
-}
-
-func (v *View) LoadLayouts() {
-	layoutsPath := v.GetLayoutsPath()
-    err := filepath.Walk(layoutsPath, func(path string, info os.FileInfo, err error) error {
-        if err != nil {
-            return err
-        }
-        if !info.IsDir() {
-            content, err := os.ReadFile(path)
-
-            if err != nil {
-                return err
-            }
-            name := filepath.ToSlash(path)
-			log.Println("Loading layout:", name)
-			fullName := filepath.Join(v.layoutsDir, info.Name())
-            _, err = v.Template.New(fullName).Parse(string(content))
-            if err != nil {
-                return err
-            }
-        }
-        return nil
-    })
-
-	if err != nil {
-        log.Fatalf("Error loading layouts from directory %s: %v", layoutsPath, err)
-    }
-}
-
-func (v *View) RenderTemplate(w http.ResponseWriter, name string, data ...interface{}) {
-    var useData interface{}
-    if len(data) > 0 {
-        useData = data[0]
-		if _, ok := useData.(interface{}); !ok {
-            log.Println("Warning: data[0] is not of type interface{}")
-            useData = struct{}{} // Provide a default empty struct if data is not of the expected type
-        }
-    } else {
-        useData = struct{}{} // Provide a default empty struct if data is nil
-    }
-	log.Println("Available templates:", v.Template.DefinedTemplates())
-	tmpl := v.Template.Lookup(name)
-    if tmpl == nil {
-        http.Error(w, "The template does not exist.", http.StatusInternalServerError)
-        return
-    }
-    err := tmpl.Execute(w, data)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
-}
-
-func (v *View) GetComponentsPath() string {
-	relComponents := filepath.Join(v.templatesDir, v.componentsDir)
-
-	return v.GetAbsPath(relComponents)
-}
-
-func (v *View) GetLayoutsPath() string {
-	relLayouts := filepath.Join(v.templatesDir, v.layoutsDir)
-
-	return v.GetAbsPath(relLayouts)
-}
-
-func (v *View) GetTemplatesPath() string {
-	return v.GetAbsPath(v.templatesDir)
-}
-
-func (v *View) GetViewsPath() string {
-	relViews := filepath.Join(v.templatesDir, v.viewsDir)
-
-	return v.GetAbsPath(relViews)
-}
-
-func (v *View) GetContentPath() string {
-	relContent := filepath.Join(v.GetViewsPath(), v.contentDir)
-
-	return v.GetAbsPath(relContent)
-}
-
-func (v *View) GetAbsPath(relPath string) string {
-	absPath, err := filepath.Abs(relPath)
-
-	if err != nil {
-		log.Fatal(err)
+		log.Println("Error executing template:", err)
 	}
 
-	return absPath
+	log.Println("PAGE CONTENT", viewContent.String())
+	td := ViewData{
+        Props:   struct{}{}, // Possibly to load other view data
+        Content: template.HTML(viewContent.String()),
+    }
+
+	log.Println("DEFINED TEMPLATES:", v.Template.DefinedTemplates())
+	return v.Template.ExecuteTemplate(r, v.Layout, td)
 }
-func (v *View) GetViewsDir() string {
-	return v.viewsDir
-}
+
