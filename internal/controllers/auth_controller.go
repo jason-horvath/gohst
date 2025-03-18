2071d40ecf6d4199fc9279276255dca1bec224dc
Jason Horvath <jason@greaterdevelopment.com>
2025-03-18 01:18:50 -0400

    Using data to render components

--- internal/controllers/auth_controller.go
+++ internal/controllers/auth_controller.go
@@ -2,6 +2,8 @@ package controllers
 
 import (
 	"net/http"
+
+	"gohst/internal/types"
 )
 type AuthController struct {
 	*BaseController
@@ -17,5 +19,48 @@ func NewAuthController() *AuthController {
 
 func (c *AuthController) Login(w http.ResponseWriter, r *http.Request) {
 	c.Init(w, r)
-	c.html.RenderView(w, "auth/login.html")
+
+	type LoginPageData struct {
+		Test string
+		Label types.Label
+		Form types.Form
+	}
+
+	data := LoginPageData{
+		Test: "This is a test",
+		Label: types.Label{
+			For: "email",
+			Text: "Email Address",
+		},
+		Form: types.Form{
+			Method: "POST",
+			Action: "/login", // Adjust as needed
+			Fieldset: types.Fieldset{
+				"email": types.Field{
+					Input: types.Text{
+						Name: "email",
+						Type: "email",
+						Placeholder: "Enter your email.",
+					},
+					Label: types.Label{For: "email", Text: "Email"},
+				},
+				"password": types.Field{
+					Input: types.Text{
+						Name: "password",
+						Type: "password",
+						Placeholder: "Enter your password.",
+					},
+					Label: types.Label{For: "password", Text: "Password"},
+				},
+			},
+			Buttons: map[string]types.Button{
+				"submit": {
+					Type: "submit",
+					Text: "Login",
+				},
+			},
+		},
+	}
+
+	c.view.Render(w, "auth/login", data)
 }
