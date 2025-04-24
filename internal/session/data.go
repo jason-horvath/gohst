58ce34a713362f4b8f2e94715c5033cf2b6b9ed9
Jason Horvath <jason@greaterdevelopment.com>
2025-04-23 22:20:24 -0400

    add session data

--- internal/session/data.go
+++ internal/session/data.go
@@ -1,14 +1,17 @@
 package session
 
 import (
+	"context"
 	"gohst/internal/config"
 	"time"
 )
 
 // SessionData stores session values
 type SessionData struct {
+	ID 		string					`json:"id"`
 	Values  map[string]interface{} 	`json:"values"`
 	Expires time.Time				`json:"expires"`
+	manager SessionStore			`json:"-"`
 }
 
 // GetSessionLength retrieves the session duration from ENV and returns it as time.Duration
@@ -25,4 +28,26 @@ func GetSessionLength() time.Duration {
 
 	// Convert minutes to time.Duration
 	return time.Duration(sessionLength) * time.Minute
+}
+
+// Set a session value through the session manager to maintain one source of truth
+func (sd *SessionData) Set(key string, value any)  {
+    sd.Values[key] = value
+    sd.manager.SetValue(sd.ID, key, value);
+	updated, _ := sd.manager.GetSessionByID(context.Background(), sd.ID)
+	sd.Values = updated.Values
+}
+
+// Get Session
+func (sd *SessionData) Get(key string) (any, bool) {
+    val, ok := sd.manager.GetValue(sd.ID, key)
+    return val, ok
+}
+
+func (sd *SessionData) CSRF() (string, bool) {
+	val, ok := sd.manager.GetValue(sd.ID, string(CSRFKey))
+	if !ok {
+		return "", false
+	}
+	return val.(string), true
 }
