a89421c3e98c5092db1707d7567242461d68c240
Jason Horvath <jason.horvath@greaterdevelopment.com>
2025-06-03 17:39:00 -0400

    adding test value

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -12,6 +12,7 @@ func Auth(next http.Handler) http.Handler {
 		sess := session.FromContext(r.Context())
 		log.Println("AUTHORIZATION")
 		log.Println("Session ID in Auth middleware using context session wrapper:", sess.ID())
+		sess.Set("Username", "Jason From Session Set update") // Example of setting a user in the session
 		// sm.SetValue(sessionId, "Authorized", "Is Jason")
 		// Add authentication logic here
 		// For now, just pass the request
