4c1ec3d54ba586bf575601f780c036223bc340d1
Jason Horvath <jason.horvath@greaterdevelopment.com>
2025-06-20 23:16:24 -0400

    moved logout to require the auth middleware which still have to be worked out

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -8,6 +8,7 @@ import (
 	"gohst/internal/session"
 )
 
+// Set up all routes and return the mux for the application.
 func SetupRoutes() http.Handler {
 	mainMux := http.NewServeMux()
 
@@ -21,32 +22,49 @@ func SetupRoutes() http.Handler {
 	mainMux.Handle("/auth/", http.StripPrefix("/auth", authRoutes)) // Auth routes
 	mainMux.Handle("/", publicRoutes) // Public routes
 
-
-
 	return mainMux
 }
 
+// auth related routes
 func setupAuthRoutes() http.Handler {
 	mux := http.NewServeMux()
 	auth := controllers.NewAuthController()
 
-	// Authentication routes
-	mux.HandleFunc("GET /login", auth.Login)                // Login page
+	// Authentication routes (for guests)
+	mux.HandleFunc("GET /login", auth.Login)               // Login page
 	mux.HandleFunc("POST /login", auth.HandleLogin)        // Handle login form submission
 	mux.HandleFunc("GET /register", auth.Register)         // Registration page
 	mux.HandleFunc("POST /register", auth.HandleRegister)  // Handle registration form submission
-	mux.HandleFunc("POST /logout", auth.HandleLogout)      // Logout action
 
-	// Apply middleware
-	return middleware.Chain(
+	guestRoutes := middleware.Chain(
 		mux,
 		session.SM.SessionMiddleware,
 		middleware.CSRF,
 		middleware.Logger,
 		middleware.Guest, // Only allow non-authenticated users
 	)
+
+	// Auth related routes that are require authenticated-only
+	authMux := http.NewServeMux()
+	authMux.HandleFunc("POST /logout", auth.HandleLogout) // Logout action
+
+	authRoutes := middleware.Chain(
+		authMux,
+		session.SM.SessionMiddleware,
+		middleware.CSRF,
+		middleware.Logger,
+		middleware.Auth, // Only allow authenticated users
+	)
+
+	// Combine both guest and auth routes under a parent mux
+	parentMux := http.NewServeMux()
+	parentMux.Handle("/", guestRoutes)
+	parentMux.Handle("/logout", authRoutes)
+
+	return parentMux
 }
 
+// Public routes meant for all users, authenticated or not
 func setupPublicRoutes() http.Handler {
 	mux := http.NewServeMux()
     pages := controllers.NewPagesController()
