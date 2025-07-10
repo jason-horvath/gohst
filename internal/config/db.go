3cc35c9bcf04c1f421032934ac1b313a93d2da2c
Jason Horvath <jason.horvath@greaterdevelopment.com>
2025-07-09 21:50:10 -0400

    interface for adding multiple configs

--- internal/config/db.go
+++ internal/config/db.go
@@ -8,6 +8,45 @@ type DatabaseConfig struct {
 	DBName		string
 }
 
+type DatabaseConfigPool struct {
+	configs map[string]*DatabaseConfig
+}
+
+func NewDatabaseConfigPool() *DatabaseConfigPool {
+	return &DatabaseConfigPool{
+		configs: make(map[string]*DatabaseConfig),
+	}
+}
+
+func (p *DatabaseConfigPool) Add(name string, config *DatabaseConfig) {
+	p.configs[name] = config
+}
+
+func (p *DatabaseConfigPool) Get(name string) (*DatabaseConfig, bool) {
+	config, exists := p.configs[name]
+	return config, exists
+}
+
+func (p *DatabaseConfigPool) GetOrDefault(name string, defaultConfig *DatabaseConfig) *DatabaseConfig {
+	if config, exists := p.configs[name]; exists {
+		return config
+	}
+	return defaultConfig
+}
+
+func (p *DatabaseConfigPool) Has(name string) bool {
+	_, exists := p.configs[name]
+	return exists
+}
+
+func (p *DatabaseConfigPool) Names() []string {
+	names := make([]string, 0, len(p.configs))
+	for name := range p.configs {
+		names = append(names, name)
+	}
+	return names
+}
+
 var DB *DatabaseConfig
 
 const DB_DEFAULT_PORT = 3306
