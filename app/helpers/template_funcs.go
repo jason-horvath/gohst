e0ad82f8b080e012dd0537002555680e63cf7879
Jason Horvath <jason.horvath@greaterdevelopment.com>
2025-07-10 22:11:23 -0400

    move template fiuncs

--- app/helpers/template_funcs.go
+++ app/helpers/template_funcs.go
@@ -2,11 +2,6 @@ package helpers
 
 import (
 	"html/template"
-	"strings"
-	"time"
-
-	"golang.org/x/text/cases"
-	"golang.org/x/text/language"
 
 	appConfig "gohst/app/config"
 )
@@ -14,33 +9,26 @@ import (
 // AppTemplateFuncs returns application-specific template functions
 func AppTemplateFuncs() template.FuncMap {
 	return template.FuncMap{
-		// App-specific functions
+		// App identity
 		"appName":        func() string { return appConfig.App.Name },
 		"appVersion":     func() string { return appConfig.App.Version },
 		"isMaintenanceMode": func() bool { return appConfig.IsMaintenanceMode() },
+
+		// App environment
+		"isDevelopment": func() bool { return appConfig.IsDevelopment() },
+        "isProduction":  func() bool { return appConfig.IsProduction() },
+        "url":           func() string { return appConfig.App.URL },
 
 		// Feature flag helpers
 		"canRegister":    func() bool { return appConfig.App.Features.EnableRegistration },
 		"hasProfiles":    func() bool { return appConfig.App.Features.EnableUserProfiles },
 		"hasNotifications": func() bool { return appConfig.App.Features.EnableNotifications },
 
-		// Utility functions
-		"formatDate":     func(t time.Time) string { return t.Format("January 2, 2006") },
-		"truncate":       func(s string, length int) string {
-			if len(s) <= length {
-				return s
-			}
-			return s[:length] + "..."
-		},
-		"upper":          strings.ToUpper,
-		"lower":          strings.ToLower,
-		"title":          func(s string) string { return cases.Title(language.English).String(s) },
-
-		// Upload helpers
+		// Upload configuration helpers
 		"maxUploadSize":  func() int64 { return appConfig.App.Upload.MaxFileSize },
 		"uploadPath":     func() string { return appConfig.App.Upload.UploadPath },
 
-		// Pagination helpers
+		// Pagination configuration helpers
 		"defaultPageSize": func() int { return appConfig.App.Pagination.DefaultLimit },
 		"maxPageSize":     func() int { return appConfig.App.Pagination.MaxLimit },
 	}
