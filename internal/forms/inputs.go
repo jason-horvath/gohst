7dbdcc5438ffae4cbae79feb6b67b4e4754384bd
Jason Horvath <jason.horvath@greaterdevelopment.com>
2025-12-10 11:26:19 -0500

    use new propos

--- internal/forms/inputs.go
+++ internal/forms/inputs.go
@@ -48,3 +48,17 @@ type Select struct {
     Label   Label
     Options []SelectOption
 }
+
+// File represents an HTML file input element
+type File struct {
+    Input       Input
+    Name        string
+    ID          string
+    Type        string
+    Errors      []string
+    Label       Label
+    Accept      string   // MIME types or file extensions (e.g., "image/*", ".jpg,.png")
+    Multiple    bool     // Allow multiple file selection
+    MaxSize     int64    // Maximum file size in bytes
+    Placeholder string   // Helper text for file selection
+}
